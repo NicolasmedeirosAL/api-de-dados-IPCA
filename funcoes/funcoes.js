9f11ec047c2dae65e202b8b6ed08b54967878b0a
Nicolas Medeiros <nick.medeiros.758660@gmail.com>
2023-08-10 21:16:53 -0300

    correção dos parametros nas funções

--- funcoes/funcoes.js
+++ funcoes/funcoes.js
@@ -1,13 +1,13 @@
 import historicoInflacao from '../dados/dados.js'
 
-export const buscarIpcaPorAno = anoIPCA => {
-  const anoFormatado = parseInt(anoIPCA)
+export const buscarIpcaPorAno = anoIpca => {
+  const anoFormatado = parseInt(anoIpca)
   const ipcaFiltrado = historicoInflacao.filter(ipca => ipca.ano === anoFormatado)
   return ipcaFiltrado
 }
 
-export const buscarPorId = idipca => {
-  const idFormatado = parseInt(idipca)
+export const buscarPorId = idIpca => {
+  const idFormatado = parseInt(idIpca)
   const idFiltrado = historicoInflacao.find(ipca => ipca.id === idFormatado)
   return idFiltrado
 }
